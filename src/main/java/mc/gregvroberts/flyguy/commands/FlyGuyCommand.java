0ff1020162e610867f7b51e5b72c390d8545fc25
Gregory Roberts <roberts.gregory@heb.com>
2021-09-15 14:23:26 -0500

    Added disable flight feature

--- src/main/java/mc/gregvroberts/flyguy/commands/FlyGuyCommand.java
+++ src/main/java/mc/gregvroberts/flyguy/commands/FlyGuyCommand.java
@@ -6,7 +6,12 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+
 public class FlyGuyCommand implements CommandExecutor {
+
+    // Used to track which players are currently allowed to fly.
+    private ArrayList<Player> listOfFlyingPlayers = new ArrayList<>();
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -15,11 +20,17 @@ public class FlyGuyCommand implements CommandExecutor {
             Player player = (Player) sender;
 
             if (player.hasPermission("flyguy.fly")) {
-                player.setAllowFlight(true);
-                player.sendMessage(ChatColor.AQUA + "You can now fly. Be free!!!");
+                if (listOfFlyingPlayers.contains(player)) {
+                    listOfFlyingPlayers.remove(player);
+                    player.setAllowFlight(false);
+                    player.sendMessage(ChatColor.AQUA + "You cannot fly anymore.");
+                } else if (!listOfFlyingPlayers.contains(player)) {
+                    listOfFlyingPlayers.add(player);
+                    player.setAllowFlight(true);
+                    player.sendMessage(ChatColor.AQUA + "You can now fly. Be free!!!");
+                }
             }
         }
-
         return true;
     }
 }
